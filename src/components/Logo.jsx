e24a06bab45569c114b3940a587b9b59c53c3e88
Wooseok Sung <tjddntjr1@gmail.com>
2026-04-30 22:52:25 +0900

    Update logo with 30:70 ratio and change global font to Gothic A1

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -10,7 +10,9 @@ export function Logo({ className = "", width = 28, height = 28, ...props }) {
       aria-hidden="true"
       {...props}
     >
-      <rect x="15" y="20" width="70" height="16" />
+      {/* 30% Immediate Release / 70% Extended Release Ratio */}
+      <rect x="15" y="20" width="21" height="16" opacity="0.4" />
+      <rect x="36" y="20" width="49" height="16" />
       <polygon points="50,36 20,80 80,80" />
     </svg>
   );
